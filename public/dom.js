fc0a625c5b156d5236bc0f62ecc79e5c89534e65
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-04 15:02:32 +0000

    adding files for express challenge - basic comlpete

---
+++ public/dom.js
@@ -0,0 +1,6 @@
+export const inputName = document.getElementById(`inputName`);
+export const buttonCreate = document.getElementById(`buttonCreate`);
+export const listOutput = document.getElementById(`listOutput`);
+export const idInput = document.getElementById(`idInput`);
+export const GetIdButton = document.getElementById(`GetIdButton`);
+export const idOutput = document.getElementById(`idOutput`);
